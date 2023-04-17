6d13a1bcfa20cd30f0710def899746ac6deeb5a5
UyenNhi <nhitran@orgos.net>
2023-04-17 13:58:44 +0700

    first commit

---
+++ build/precache-manifest.67ec5589788a313fa92720defcbf89cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c39aab2a5b8eac413ce5b90f2cd0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082bcc0496e6d72f672e",
+    "url": "/static/css/2.228ae3e0.chunk.css"
+  },
+  {
+    "revision": "64c209cbbc7cb7bd740a",
+    "url": "/static/css/main.afffd827.chunk.css"
+  },
+  {
+    "revision": "082bcc0496e6d72f672e",
+    "url": "/static/js/2.79ffdcf8.chunk.js"
+  },
+  {
+    "revision": "7141b9eb55418e59519f45891008aeee",
+    "url": "/static/js/2.79ffdcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c209cbbc7cb7bd740a",
+    "url": "/static/js/main.7ebba2fc.chunk.js"
+  },
+  {
+    "revision": "b651ffb7292b4edb4dde",
+    "url": "/static/js/runtime-main.184f705d.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
---
+++ src/config.js
@@ -0,0 +1,130 @@
+let config = {};
+config.contract = {
+    address: "0x948f39835345a2a2Ce19DB649907C02A9d3aAC35",
+    abi: [
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "getQuote",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "currentQuote",
+                    "type": "string"
+                },
+                {
+                    "internalType": "address",
+                    "name": "currentOwner",
+                    "type": "address"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "nonces",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "quote",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "newQuote",
+                    "type": "string"
+                }
+            ],
+            "name": "setQuote",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "userAddress",
+                    "type": "address"
+                },
+                {
+                    "internalType": "string",
+                    "name": "newQuote",
+                    "type": "string"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "r",
+                    "type": "bytes32"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "s",
+                    "type": "bytes32"
+                },
+                {
+                    "internalType": "uint8",
+                    "name": "v",
+                    "type": "uint8"
+                }
+            ],
+            "name": "setQuoteMeta",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+}
+
+
+module.exports = { config }
